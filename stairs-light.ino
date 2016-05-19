1c43009166d796a8fe72bdbb97f565f0eac0151c
Max Home <max@huzm.ru>
2016-05-20 00:33:30 +0300

    Included new sketch file

---
+++ stairs-light.ino
@@ -0,0 +1,26 @@
+
+unsigned char pin=3;
+unsigned int r, level;
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+  analogWrite(pin, 200);  
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    r = Serial.read();
+    level = (r-48)*28; 
+    Serial.print("r = ");
+    Serial.print(r);
+    Serial.print(" / level = ");
+    Serial.println(level);
+    analogWrite(pin, level);
+  }
+  delay(100);
+}
+
+
+
